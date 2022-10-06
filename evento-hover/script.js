ff816ed4fa64f49522a59e7a7429862558510efd
maycrxyy <mauricio.munoz.gonzalez@alumnos.sip.cl>
2022-10-06 10:55:56 -0300

    sdasd

--- evento-hover/script.js
+++ evento-hover/script.js
@@ -1,13 +1,8 @@
 function addSombra(element){
-    element.classList.add("sombra");
+element.classList.add("sombra");
+
 }
-    
 function removeSombra(element){
     element.classList.remove("sombra");
-    
-    }
-function mifuncion(){
-
-    console.log("CLICKS");
     
     }
